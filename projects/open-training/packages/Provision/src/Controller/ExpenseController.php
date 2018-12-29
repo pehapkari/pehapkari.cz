9e6aa6da482353cc12e645009f0f7c85e0af7b55
Tomas Votruba <tomas.vot@gmail.com>
2018-12-29 21:14:36 +0100

    make Expense form use list

---
+++ projects/open-training/packages/Provision/src/Controller/ExpenseController.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+namespace OpenTraining\Provision\Controller;
+
+use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController;
+use OpenTraining\Provision\Data\Partner;
+use OpenTraining\Provision\Entity\Expense;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormInterface;
+
+/**
+ * @see Expense
+ */
+final class ExpenseController extends EasyAdminController
+{
+    /**
+     * @param mixed[] $entityProperties
+     */
+    protected function createEditForm($entity, array $entityProperties): FormInterface
+    {
+        $editForm = parent::createEditForm($entity, $entityProperties);
+        $this->decorateForm($editForm);
+        return $editForm;
+    }
+
+    /**
+     * @param mixed[] $entityProperties
+     */
+    protected function createNewForm($entity, array $entityProperties): FormInterface
+    {
+        $newForm = parent::createNewForm($entity, $entityProperties);
+        $this->decorateForm($newForm);
+        return $newForm;
+    }
+
+    private function decorateForm(FormInterface $form): void
+    {
+        $form->add('partner', ChoiceType::class, [
+            'choices' => [
+                // label => value
+                'Vlastník' => Partner::OWNER,
+                'Organizátor' => Partner::ORGANIZER,
+                'Školitel' => Partner::TRAINER,
+            ],
+        ]);
+    }
+}
