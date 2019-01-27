841a6dc6119606fb8f34ee3687e980991d35e577
Tomas Votruba <tomas.vot@gmail.com>
2019-01-27 13:04:56 +0100

    migrate open-training to single-repo

---
+++ public/index.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+use OpenTraining\OpenTrainingKernel;
+use Symfony\Component\Debug\Debug;
+use Symfony\Component\HttpFoundation\Request;
+
+require_once __DIR__  .'/../vendor/autoload.php';
+
+require_once __DIR__ . '/../config/bootstrap.php';
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+
+    Debug::enable();
+}
+
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+}
+
+if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
+    Request::setTrustedHosts([$trustedHosts]);
+}
+
+$kernel = new OpenTrainingKernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
