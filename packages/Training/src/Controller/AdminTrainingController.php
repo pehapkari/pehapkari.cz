e473766c89268a1dc1f837839c94d68999f1f4b3
Tomas Votruba <tomas.vot@gmail.com>
2019-03-28 13:49:54 +0100

    move php prague conference to Symfony

--- packages/Training/src/Controller/AdminTrainingController.php
+++ packages/Training/src/Controller/AdminTrainingController.php
@@ -10,7 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-final class TrainingController extends AbstractController
+final class AdminTrainingController extends AbstractController
 {
     /**
      * @var TrainingRepository
