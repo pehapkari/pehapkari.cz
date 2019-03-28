7c26ed8f448494939c023917c51d5ba9adef450e
Tomas Votruba <tomas.vot@gmail.com>
2019-03-28 03:47:55 +0100

    make ready

--- packages/Statie/src/Controller/StatiePostController.php
+++ packages/Statie/src/Controller/StatiePostController.php
@@ -2,27 +2,75 @@
 
 namespace OpenTraining\Statie\Controller;
 
+use OpenTraining\Exception\ShouldNotHappenException;
+use Psr\Http\Message\ResponseInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symplify\Statie\Generator\Generator;
+use Symplify\Statie\Renderable\File\PostFile;
 
 final class StatiePostController extends AbstractController
 {
-    // @todo redirect post url to generated markdown to twig content from
-    // _source/posts ...
+    /**
+     * @var Generator
+     */
+    private $generator;
+
+    /**
+     * @var mixed[]
+     */
+    private $authors = [];
+
+    /**
+     * @param mixed[] $authors
+     */
+    public function __construct(Generator $generator, array $authors)
+    {
+        $this->generator = $generator;
+        $this->authors = $authors;
+    }
 
     /**
      * @Route(path="/blog/", name="blog")
      */
-    public function blog()
+    public function blog(): Response
     {
+        $abstractGeneratorFiles = $this->generator->run();
+        $values = $abstractGeneratorFiles + ['authors' => $this->authors];
 
+        return $this->render('default/blog.html.twig', $values);
     }
 
     /**
-     * @Route(path="/blog/{post-slug}", name="post")
+     * @Route(path="/blog/{postSlug}", name="post", requirements={"postSlug"=".+"})
      */
-    public function post(string $postSlug)
+    public function post(string $postSlug): Response
     {
-        dump($postSlug);
+        $matchedPost = $this->matchPostSlug($postSlug);
+        if ($matchedPost === null) {
+            throw new ShouldNotHappenException();
+        }
+
+        return $this->render('default/post.html.twig', [
+            'post' => $matchedPost,
+            'authors' => $this->authors
+        ]);
+    }
+
+    private function matchPostSlug(string $postSlug): ?PostFile
+    {
+        $posts = $this->generator->run()['posts'] ?? [];
+
+        /** @var PostFile $post */
+        foreach ($posts as $post) {
+            if ($post->getRelativeUrl()  . '/' !== 'blog/' . $postSlug) {
+                continue;
+            }
+
+            return $post;
+        }
+
+        return null;
     }
 }
