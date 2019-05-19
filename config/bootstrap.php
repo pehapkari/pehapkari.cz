e46ed0195d8e228e5c2bdc8af15677fdc154e8ff
Jan Mikeš <j.mikes@me.com>
2019-05-20 00:30:45 +0200

    Debug ip address

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -16,10 +16,14 @@ $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'
 $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
 $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
 
-if (in_array($_SERVER['REMOTE_ADDR'], [
+if (isset($_COOKIE['XDEBUG_TRACE']) && in_array(getUserIpAddr(), [
     '86.49.240.47', // Jan Mikes
     '86.49.112.53', // Tomas Votruba
 ], true)) {
     $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = 1;
     $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = 'dev';
+}
+
+function getUserIpAddr(): string {
+    return $_SERVER['HTTP_CLIENT_IP'] ?? $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'];
 }
