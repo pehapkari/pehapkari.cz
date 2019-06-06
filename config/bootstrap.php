896131525f50036d2342e5b66d2fb8bef32d87d4
Jan Mikeš <j.mikes@me.com>
2019-06-07 01:27:49 +0200

    More sophisticated debugging cookie

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -16,10 +16,7 @@ $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'
 $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
 $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
 
-if (isset($_COOKIE['XDEBUG_TRACE']) && in_array(getUserIpAddr(), [
-    '86.49.240.47', // Jan Mikes
-    '86.49.112.53', // Tomas Votruba
-], true)) {
+if (isset($_COOKIE['XDEBUG_TRACE']) && !empty($_ENV['DEBUG_COOKIE']) && $_COOKIE['XDEBUG_TRACE'] === $_ENV['DEBUG_COOKIE']) {
     $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = 1;
     $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = 'dev';
 }
