d0060de04cf24facbf02b6ebd3cafc670f0037a1
Tomas Votruba <tomas.vot@gmail.com>
2019-06-19 18:12:29 +0200

    add dual group to trainings repository for feedback terms

--- packages/Training/src/Repository/TrainingRepository.php
+++ packages/Training/src/Repository/TrainingRepository.php
@@ -43,6 +43,7 @@ final class TrainingRepository
             ->setParameter(':nextWeek', DateTime::from('+ 7 days'))
             // has at least one registration
             ->join('tt.registrations', 'tr')
+            ->addGroupBy('t.id')
             ->addGroupBy('tt.startDateTime')
             // prefer closest to now - so people rating training at the day of training will get the date fo training :)
             // see https://stackoverflow.com/questions/21490993/expected-known-function-got-timedifffunction-not-found-in-doctrine-orm
