64b626d02efaef31331e1e40a015ec8e7bd7d65f
Tomas Votruba <tomas.vot@gmail.com>
2019-07-19 15:52:43 +0200

    wip

--- packages/Training/src/Admin/Controller/AdminTrainingTermController.php
+++ packages/Training/src/Admin/Controller/AdminTrainingTermController.php
@@ -3,8 +3,11 @@
 namespace Pehapkari\Training\Admin\Controller;
 
 use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController;
+use Pehapkari\Exception\ShouldNotHappenException;
 use Pehapkari\Marketing\MarketingEventsFactory;
+use Pehapkari\Training\Entity\TrainingTerm;
 use Pehapkari\Training\Repository\TrainingTermRepository;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @see \Pehapkari\Training\Entity\TrainingTerm
@@ -30,6 +33,19 @@ final class AdminTrainingTermController extends EasyAdminController
     }
 
     /**
+     * @Route(path="/admin/provision/send-email-to-trainer-with-amount-to-invoice/{id}", name="send_email_to_trainer_with_amount_to_invoice")
+     */
+    public function sendEmailToTrainerWithAmountToInvoice(TrainingTerm $trainingTerm)
+    {
+        $trainerEmail = $trainingTerm->getTrainer()->getEmail();
+        $this->ensureTrainerEmailIsSet($trainingTerm, $trainerEmail);
+
+        dump($trainerEmail);
+        dump($trainingTerm);
+        die;
+    }
+
+    /**
      * @param int[] $ids
      */
     public function generateMarketingEventsBatchAction(array $ids): void
@@ -49,5 +65,18 @@ final class AdminTrainingTermController extends EasyAdminController
 
             $this->addFlash('success', sprintf('Kampaň pro "%s" byla vytvořena', (string) $trainingTerm));
         }
+    }
+
+    private function ensureTrainerEmailIsSet(TrainingTerm $trainingTerm, ?string $trainerEmail): void
+    {
+        if ($trainerEmail !== null) {
+            return;
+        }
+
+        throw new ShouldNotHappenException(sprintf(
+            'Cannot send email to "%s" for "%s" training. He or she have no email :(',
+            $trainingTerm->getTrainer()->getName(),
+            (string)$trainingTerm
+        ));
     }
 }
