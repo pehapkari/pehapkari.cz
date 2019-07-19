e92d52b3ce8de73ab074c0f958ed893b6c11a24d
Jan Mikeš <j.mikes@me.com>
2019-07-19 23:55:56 +0200

    Deploy scripts

---
+++ bin/deploy-new.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+eval $(ssh-agent -s)
+mkdir -p ~/.ssh
+ssh-keyscan -H pehapkari.cz >> ~/.ssh/known_hosts
+echo "$DEPLOY_PRIVATE_KEY" | ssh-add - > /dev/null
+ssh root@pehapkari.cz "cd /projects/pehapkari.cz && ./run-new.sh"
