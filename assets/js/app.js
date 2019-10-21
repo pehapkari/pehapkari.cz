6eeec60f2756145de5f48e4b7113b7a26a556bef
Tomas Votruba <tomas.vot@gmail.com>
2019-10-21 15:54:17 +0200

    remove highlight js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,2 @@
 // @see https://github.com/symfony/demo/tree/2a330a3bbb827e145b6a49a3665b3b45a4235dee/assets/js
 import 'jquery'
-
-// loads the code syntax highlighting library
-import './highlight.js';
