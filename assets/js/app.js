59237bce6b2164920611a7dd9f62fc59fd00b501
Tomas Votruba <tomas.vot@gmail.com>
2019-10-25 11:31:43 +0200

    add typed, allow invokable

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,5 @@
 // @see https://github.com/symfony/demo/tree/2a330a3bbb827e145b6a49a3665b3b45a4235dee/assets/js
 import 'jquery'
+
+// @see https://github.com/mattboldt/typed.js
+import 'typed.js'
---
+++ packages/Training/src/Controller/BecomeTrainerController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pehapkari\Training\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class BecomeTrainerController extends AbstractController
+{
+    /**
+     * @Route(path="/zacni-skolit/", name="become_trainer")
+     */
+    public function __invoke(): Response
+    {
+        return $this->render('training/become_trainer.twig');
+    }
+}
