295c4b32a00619c21eae758819c46a47ac79053a
Tomas Votruba <tomas.vot@gmail.com>
2019-10-25 19:46:55 +0200

    add nereast meetup

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Pehapkari\Controller;
 
+use Pehapkari\DataProvider\NearestMeetupProvider;
 use Pehapkari\Statie\OragnizerProvider;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,10 +15,11 @@ final class HomepageController extends AbstractController
     /**
      * @Route(path="/", name="homepage")
      */
-    public function __run(OragnizerProvider $oragnizerProvider): Response
+    public function __run(OragnizerProvider $oragnizerProvider, NearestMeetupProvider $nearestMeetupProvider): Response
     {
         return $this->render('homepage/homepage.twig', [
             'organizers' => $oragnizerProvider->provide(),
+            'nearest_meetup' => $nearestMeetupProvider->provide(),
         ]);
     }
 }
