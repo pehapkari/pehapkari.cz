e52e0ddf0c5c49518a4025f266812e3b9b41447a
Tomas Votruba <tomas.vot@gmail.com>
2019-11-02 08:09:37 +0100

    check sent feedback

--- packages/Training/src/Controller/AdminSendFeedbackRequestController.php
+++ packages/Training/src/Controller/AdminSendFeedbackRequestController.php
@@ -7,21 +7,53 @@ namespace Pehapkari\Training\Controller;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController;
 use Pehapkari\Mailer\PehapkariMailer;
 use Pehapkari\Training\Entity\TrainingTerm;
+use Pehapkari\Training\Repository\TrainingTermRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class AdminSendFeedbackRequestController extends EasyAdminController
 {
+    /**
+     * @var PehapkariMailer
+     */
+    private $pehapkariMailer;
+
+    /**
+     * @var TrainingTermRepository
+     */
+    private $trainingTermRepository;
+
+    public function __construct(PehapkariMailer $pehapkariMailer, TrainingTermRepository $trainingTermRepository)
+    {
+        $this->pehapkariMailer = $pehapkariMailer;
+        $this->trainingTermRepository = $trainingTermRepository;
+    }
 
     /**
      * @Route(path="admin/send-training-term-feedback-form/{id}", name="send_traning_term_feedback_form")
      */
-    public function __invoke(TrainingTerm $trainingTerm, PehapkariMailer $pehapkariMailer): Response
+    public function __invoke(TrainingTerm $trainingTerm): Response
     {
-        $pehapkariMailer->sendFeedbackFormToRegistrations($trainingTerm);
+        if ($trainingTerm->areFeedbackEmailsSent() === false) {
+            $this->sendFeedbackEmails($trainingTerm);
+        } else {
+            $this->addFlash('warning', 'Email s feedbacky už byl poslán');
+        }
 
         return $this->redirectToRoute('training_term_organization', [
-            'id' => $trainingTerm->getId()
+            'id' => $trainingTerm->getId(),
         ]);
+    }
+
+    private function sendFeedbackEmails(TrainingTerm $trainingTerm): void
+    {
+        $this->pehapkariMailer->sendFeedbackFormToRegistrations($trainingTerm);
+        $trainingTerm->setAreFeedbackEmailsSent(true);
+
+        $this->trainingTermRepository->save($trainingTerm);
+
+        foreach ($trainingTerm->getRegistrations() as $registration) {
+            $this->addFlash('success', sprintf('Email s feedbackem poslán na "%s"', $registration->getEmail()));
+        }
     }
 }
