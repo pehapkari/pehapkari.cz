6d192980980c772d352bd132827f8b224a3736fd
Tomas Votruba <tomas.vot@gmail.com>
2019-11-02 15:45:36 +0100

    add calendar links + place

---
+++ src/ValueObject/Place.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pehapkari\ValueObject;
+
+final class Place
+{
+    /**
+     * @var string
+     */
+    public const PRAGUE_ADDRESS = 'U Pergamenky 1145/12, Praha 7, 170 00';
+}
