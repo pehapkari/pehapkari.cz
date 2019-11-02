a96bc343b53331e11d42e80b3c720b384638bb91
Tomas Votruba <tomas.vot@gmail.com>
2019-11-02 10:08:05 +0100

    add PostRepository

--- src/Controller/BlogPostController.php
+++ src/Controller/BlogPostController.php
@@ -4,53 +4,31 @@ declare(strict_types=1);
 
 namespace Pehapkari\Controller;
 
+use Pehapkari\Repository\PostRepository;
 use Pehapkari\Statie\AuthorsProvider;
-use Pehapkari\Statie\PostsProvider;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symplify\Statie\Renderable\File\PostFile;
 
 final class BlogPostController extends AbstractController
 {
     /**
-     * @var PostsProvider
-     */
-    private $postsProvider;
-
-    public function __construct(PostsProvider $postsProvider)
-    {
-        $this->postsProvider = $postsProvider;
-    }
-
-    /**
      * @Route(path="blog/{postSlug}", name="post", requirements={"postSlug"=".+"})
      */
-    public function __invoke(string $postSlug, AuthorsProvider $authorsProvider): Response
-    {
-        $matchedPost = $this->matchPostSlug($postSlug);
-        if ($matchedPost === null) {
+    public function __invoke(
+        string $postSlug,
+        AuthorsProvider $authorsProvider,
+        PostRepository $postRepository
+    ): Response {
+        $post = $postRepository->findBySlug($postSlug);
+        if ($post === null) {
             throw $this->createNotFoundException(sprintf("Post with slug '%s' not found", $postSlug));
         }
 
         return $this->render('blog/post.twig', [
-            'post' => $matchedPost,
+            'post' => $post,
             'authors' => $authorsProvider->provide(),
-            'title' => $matchedPost->getConfiguration()['title'] ?? null,
+            'title' => $post->getConfiguration()['title'] ?? null,
         ]);
-    }
-
-    private function matchPostSlug(string $postSlug): ?PostFile
-    {
-        $posts = $this->postsProvider->provide();
-
-        /** @var PostFile $post */
-        foreach ($posts as $post) {
-            if ($post->getRelativeUrl() . '/' === 'blog/' . $postSlug || $post->getRelativeUrl() === 'blog/' . $postSlug) {
-                return $post;
-            }
-        }
-
-        return null;
     }
 }
